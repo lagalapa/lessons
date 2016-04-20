f755c095e2e7bfaed43dc30c6b061939401079e3
lagalapa <lagalapa@gmail.com>
2016-04-20 14:25:54 +0000

    Rubocop check

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -1,16 +1,16 @@
-print "Enter a: "
+print 'Enter a: '
 a = gets.to_f
-print "Enter b: "
+print 'Enter b: '
 b = gets.to_f
-print "Enter c: "
+print 'Enter c: '
 c = gets.to_f
 roots = []
 discriminant = b**2 - 4 * a * c
 if discriminant >= 0
   roots << (-b + Math.sqrt(discriminant)) / (2 * a)
   roots << (-b - Math.sqrt(discriminant)) / (2 * a)
-else roots << "no roots"
+else roots << 'no roots'
 end
 
-puts "Roots: "
+puts 'Roots: '
 roots.uniq.each { |root| puts root }
--- lesson1/rectangular_triangle.rb
+++ lesson1/rectangular_triangle.rb
@@ -1,25 +1,25 @@
 properties = []
 sides = []
 
-print "Enter side a: "
+print 'Enter side a: '
 sides << gets.to_f
-print "Enter side b: "
+print 'Enter side b: '
 sides << gets.to_f
-print "Enter side c: "
+print 'Enter side c: '
 sides << gets.to_f
 
 if sides.uniq.size == 1
-  properties << "equilateral"
+  properties << 'equilateral'
 elsif sides.uniq.size == 2
-  properties << "isosceles"
+  properties << 'isosceles'
 end
 
-if sides.count(sides.max) == 1 
+if sides.count(sides.max) == 1
   max_side = sides.delete(sides.max)
-  properties << "rectangular" if max_side**2 == sides[0]**2 + sides[1]**2
+  properties << 'rectangular' if max_side**2 == sides[0]**2 + sides[1]**2
 end
 
-properties << "common" if properties.empty?
+properties << 'common' if properties.empty?
 
-puts "The triangle is:"
+puts 'The triangle is:'
 properties.each { |x| puts x }
