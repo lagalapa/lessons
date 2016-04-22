878b0bb4fce04080767ddb212e3500d1adee4663
lagalapa <lagalapa@gmail.com>
2016-04-22 08:53:57 +0000

    Fix shadowing

--- lesson2/1.rb
+++ lesson2/1.rb
@@ -1,6 +1,6 @@
 months = %w( January February March April May June July August September
              October November December )
-days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days_in_months = Hash[months.zip(days)]
+days_number = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days_in_months = Hash[months.zip(days_number)]
 puts 'The months in which 30 days:'
 days_in_months.each { |month, days| puts month if days == 30 }
